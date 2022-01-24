4fa5cfa1e355b402476e2c106726516d2a7e7021
User <User@VOSTRO-PC>
2022-01-24 23:13:22 +0530

    first commit of userA

---
+++ src/test/java/comcast_sample_git/DemoGit.java
@@ -0,0 +1,11 @@
+package comcast_sample_git;
+
+import org.testng.annotations.Test;
+
+public class DemoGit {
+	@Test
+	public void samplerepo() {
+		System.out.println("UserA first commit");
+	}
+
+}
